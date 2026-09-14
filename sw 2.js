8edf51576efadd497c4daad05b92b2d100de5e26
homestaymochimoon-spec <homestaymochimoon@gmail.com>
2026-09-14 15:10:17 +0800

    Delete sw 2.js

--- sw 2.js
+++
@@ -1,5 +0,0 @@
-const CACHE='mochi-moon-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
